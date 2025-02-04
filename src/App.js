3e03ff25d7711d3dfc7fd8b63335933efd58b2de
Thang0226 <thang.nd0226@gmail.com>
2025-02-04 09:55:14 +0700

    finish

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Users from "./components/Users";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
